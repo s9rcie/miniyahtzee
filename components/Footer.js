f4a0ff71fc9415e288e128fac7090dd2b4cc1d0c
s9rcie <jesse.ruonakoski@hotmail.com>
2024-02-28 21:00:04 +0200

    updated yahtzee

---
+++ components/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import { Text, View } from 'react-native'
+import styles from '../style/style'
+
+export default function Footer() {
+  return (
+    <View style={styles.footer}>
+      <Text style={styles.author}>Jesse Ruonakoski</Text>
+    </View>
+  )
+}
